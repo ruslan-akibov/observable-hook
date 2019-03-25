0f419ef4b0a894864a372ecf717c395459e27952
Ruslan Akibov <rakibov@wiley.com>
2019-03-25 18:25:36 +0300

    'extend' sample

--- src/store/SampleStore.js
+++ src/store/SampleStore.js
@@ -1,14 +1,13 @@
-export default class SampleStore {
+class SampleStoreBaseClass {
     value = 0;
-    arrValue = [];
-
-    // 'static' props will be skipped in serialization (and reactions as well)
-    // but you still can access it with '<Object-Instance>.constructor.<Prop-Name>'
-    static bigData = {};
 
     incValue() {
         this.value++;
     }
+}
+
+export default class SampleStore extends SampleStoreBaseClass {
+    arrValue = [];
 
     pushValue() {
         this.arrValue.push(this.value);
